2ba49a147358e1a3ea21674a9da4b93525a19f95
Vince <Vince@93yy.top>
2024-05-31 13:42:06 +0800

    msg:init project

---
+++ Source/TimePower/TimeManager.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TimeManager.h"
+
+// Sets default values
+ATimeManager::ATimeManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+float ATimeManager::GetTimeFactor()
+{
+	return CurrentTimeFactor;
+}
+
+// Called when the game starts or when spawned
+void ATimeManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Apply normal time
+	CurrentTimeFactor = NormalTimeFactor;
+	
+}
+
+// Called every frame
+void ATimeManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATimeManager::BeginTimeReverse()
+{
+	CurrentTimeFactor = ReverseTimeFactor;
+}
+
+void ATimeManager::EndTimeReverse()
+{
+	CurrentTimeFactor = NormalTimeFactor;
+}
+
---
+++ Source/TimePower/TimeManager.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TimeManager.generated.h"
+
+UCLASS()
+class TIMEPOWER_API ATimeManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATimeManager();
+
+	UFUNCTION(BlueprintCallable)
+	float GetTimeFactor();
+
+	UFUNCTION(BlueprintCallable)
+	void BeginTimeReverse();
+
+	UFUNCTION(BlueprintCallable)
+	void EndTimeReverse();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	float CurrentTimeFactor = 1.0f;
+	
+	UPROPERTY(BlueprintReadWrite)
+	float NormalTimeFactor = 1.0f;
+
+	UPROPERTY(BlueprintReadWrite)
+	float ReverseTimeFactor = -3.0f;
+
+};
